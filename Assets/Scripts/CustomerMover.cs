fe5cdd7f994472ab9524df85c5c96c6fd8132888
sean brown <seanbrown7935@gmail.com>
2018-04-21 21:05:51 -0600

    Created script for moving a customer.

---
+++ Assets/Scripts/CustomerMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CustomerMover : MonoBehaviour {
+
+    public List<Vector3> destination;
+
+    NavMeshAgent agent;
+    int currentDestination = 0;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = destination[currentDestination++];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (agent.remainingDistance < agent.stoppingDistance) {
+            agent.destination = destination[currentDestination++];
+            if (currentDestination > destination.Count) {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
