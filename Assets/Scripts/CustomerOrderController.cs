d3ff3100f6a1fc3dd91678dca339592363d4687d
sean brown <seanbrown7935@gmail.com>
2018-04-21 21:06:18 -0600

    Created a script for controlling the order window and creating an order.

---
+++ Assets/Scripts/CustomerOrderController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerOrderController : MonoBehaviour {
+
+    public Canvas customerCanvas;
+
+    Order currentOrder;
+
+	// Use this for initialization
+	void Start () {
+        customerCanvas = Instantiate(customerCanvas);
+
+        GameObject panel = customerCanvas.gameObject.GetComponentInChildren<Image>().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        customerCanvas.transform.position = new Vector3(transform.position.x, customerCanvas.transform.position.y, transform.position.z);
+	}
+}
