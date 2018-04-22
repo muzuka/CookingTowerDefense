db07aa421315981329fdd487b2b568b765abb54f
sean brown <seanbrown7935@gmail.com>
2018-04-21 20:57:50 -0600

    Created script for creating a burger. Currently does all that it needs.

---
+++ Assets/Scripts/BurgerCreate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgerCreate : MonoBehaviour {
+
+    Stack<GameObject> burger;
+
+    public float burgerThickness;
+    public List<string> toppingNames;
+    public List<GameObject> toppingObjects;
+
+	// Use this for initialization
+	void Start () {
+        burger = new Stack<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addTopping(string topping)
+    {
+        GameObject burgerTopping = Instantiate(toppingObjects[toppingNames.IndexOf(topping)], transform.position + new Vector3(0.0f, burgerThickness * burger.Count, 0.0f), Quaternion.identity, transform);
+        burger.Push(burgerTopping);
+    }
+
+    public void sendBurger()
+    {
+        deleteBurger();
+    }
+
+    public void deleteBurger() {
+        while (burger.Count > 0)
+        {
+            Destroy(burger.Pop());
+        }
+    }
+}
