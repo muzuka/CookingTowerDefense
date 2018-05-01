0d3cfce414a9b7b2a3bf62d65aeed05a4a9cf39b
sean brown <seanbrown7935@gmail.com>
2018-05-01 12:31:11 -0600

    Created hotkeys and fixed up burger creation.

--- Assets/Scripts/BurgerCreate.cs
+++ Assets/Scripts/BurgerCreate.cs
@@ -5,36 +5,81 @@ using UnityEngine;
 public class BurgerCreate : MonoBehaviour {
 
     Stack<GameObject> burger;
+    Stack<string> burgerStrings;
 
     public float burgerThickness;
     public List<string> toppingNames;
     public List<GameObject> toppingObjects;
 
+    GameController gameController;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
         burger = new Stack<GameObject>();
+        burgerStrings = new Stack<string>();
+
+        gameController = FindObjectOfType<GameController>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.P)) 
+        {
+            addTopping("Patty");
+        }
+
+        if(Input.GetKeyUp(KeyCode.M))
+        {
+            addTopping("Mustard");
+        }
+
+        if(Input.GetKeyUp(KeyCode.K))
+        {
+            addTopping("Ketchup");
+        }
+
+        if(Input.GetKeyUp(KeyCode.T))
+        {
+            addTopping("TopBun");
+        }
+
+        if(Input.GetKeyUp(KeyCode.B))
+        {
+            addTopping("BottomBun");
+        }
+
+        if(Input.GetKeyUp(KeyCode.Return))
+        {
+            sendBurger();
+        }
+
+        if(Input.GetKeyUp(KeyCode.Backspace))
+        {
+            deleteBurger();
+        }
+    }
 
     public void addTopping(string topping)
     {
         GameObject burgerTopping = Instantiate(toppingObjects[toppingNames.IndexOf(topping)], transform.position + new Vector3(0.0f, burgerThickness * burger.Count, 0.0f), Quaternion.identity, transform);
         burger.Push(burgerTopping);
+        burgerStrings.Push(topping);
     }
 
     public void sendBurger()
     {
+        gameController.sendBurger(burgerStrings);
         deleteBurger();
     }
 
-    public void deleteBurger() {
+    public void deleteBurger() 
+    {
         while (burger.Count > 0)
         {
             Destroy(burger.Pop());
         }
+        burger.Clear();
+        burgerStrings.Clear();
     }
 }
