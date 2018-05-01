672a0c980e63a2fa10db5b4d5ed80d516f4710f0
sean brown <seanbrown7935@gmail.com>
2018-05-01 12:39:28 -0600

    Added constructors and a toString function.

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -5,4 +5,25 @@ public class Order {
 
     public List<string> ingredients;
 
+    public Order() 
+    {
+        ingredients = new List<string>();
+    }
+
+    public Order(List<string> ingredients) 
+    {
+        this.ingredients = ingredients;
+    }
+
+    public string toString()
+    {
+        string ret = "";
+
+        for (int i = 0; i < ingredients.Count; i++)
+        {
+            ret += ingredients[i] + ", ";
+        }
+
+        return ret;
+    }
 }
