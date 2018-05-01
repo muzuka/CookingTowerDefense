cddb0acb3185eb5c9a33b423f41d746144f70397
sean brown <seanbrown7935@gmail.com>
2018-05-01 12:34:43 -0600

    Customers now follow a list of destinations.

--- Assets/Scripts/CustomerMover.cs
+++ Assets/Scripts/CustomerMover.cs
@@ -6,23 +6,45 @@ using UnityEngine.AI;
 public class CustomerMover : MonoBehaviour {
 
     public List<Vector3> destination;
+    public GameObject marker;
+    public bool debug;
 
     NavMeshAgent agent;
     int currentDestination = 0;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = destination[currentDestination++];
+        agent.destination = destination[currentDestination];
+        if (debug)
+            marker = Instantiate(marker, destination[currentDestination], Quaternion.identity);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (agent.remainingDistance < agent.stoppingDistance) {
-            agent.destination = destination[currentDestination++];
-            if (currentDestination > destination.Count) {
-                Destroy(gameObject);
+	void Update () 
+    {
+        if (agent.remainingDistance < agent.stoppingDistance) 
+        {
+            currentDestination++;
+            if (currentDestination >= destination.Count) 
+            {
+                Debug.Log("Destroyed customer at " + currentDestination);
+                EventManager.TriggerEvent("CustomerLeft");
+                if (debug)
+                    Destroy(marker);
+                Destroy(this.gameObject);
+                return;
             }
+            agent.destination = destination[currentDestination];
+            if (debug)
+                marker.transform.position = agent.destination;
         }
 	}
+
+    void OnDestroy()
+    {
+        if (debug)
+            Destroy(marker);
+    }
 }
