7dd94f03030c2eec5a2d51c01684b7612592f261
DESKTOP-535OJ45\seanb <seanbrown7935@gmail.com>
2018-05-24 21:09:23 -0600

    Created script for handling main menu button functions.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void startButtonPressed()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void quitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
