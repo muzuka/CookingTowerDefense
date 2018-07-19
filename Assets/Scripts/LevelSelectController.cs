cc534b302697d26f5eb858a06fc2c26bcd792ee0
DESKTOP-535OJ45\seanb <seanbrown7935@gmail.com>
2018-07-19 17:22:45 -0600

    Created new LevelSelectController and added button functions.

---
+++ Assets/Scripts/LevelSelectController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectController : MonoBehaviour {
+
+    public GameObject menu;
+
+    Text title;
+    Text cost;
+
+    string[] titleList = {"Burger Joint", "Hot Dog Stand", "Taco Fiesta", "Ice Cream Stand", "Sushi Grill", "Diner", "Indian Restaurant", "Fancy Restaurant"};
+    int[] costList = { 100, 100, 200, 200, 400, 400, 500, 500};
+
+    void Start()
+    {
+        Text[] texts = menu.GetComponentsInChildren<Text>();
+
+        for (int i = 0; i < texts.Length; i++)
+        {
+            if (texts[i].name == "Title")
+            {
+                title = texts[i];
+            }
+            else if (texts[i].name == "Cost")
+            {
+                cost = texts[i];
+            }
+        }
+
+        menu.SetActive(false);
+    }
+
+    public void openMenu (int pos)
+    {
+        menu.SetActive(true);
+        title.text = titleList[pos];
+        cost.text = "$" + costList[pos].ToString();
+    }
+
+    public void closeMenu ()
+    {
+        menu.SetActive(false);
+    }
+
+    public void playGame ()
+    {
+        SceneManager.LoadScene("BurgerLevel1");
+    }
+}
